3c0227c35eec0cc5c8dad8fc23e51a7d0bbcf246
Saiee Katarkar - Blockchain Intern <saieekatarkar@gmail.com>
2026-01-21 17:10:02 +0530

    Create hello_world.py

---
+++ Week-1-Foundation/hello_world.py
@@ -0,0 +1,14 @@
+"""
+Week 1: Foundation & Setup
+Author: Saiee Katarkar
+Description:
+Basic environment verification script to confirm
+successful setup of Python and GitHub workflow.
+"""
+
+def main():
+    message = "Hello, Blockchain World!"
+    print(message)
+
+if __name__ == "__main__":
+    main()
